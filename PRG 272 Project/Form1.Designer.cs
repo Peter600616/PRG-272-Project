03d457263700311da7629b9e1e8138ca0589b139
Peter Meyer <600616@student.belgiumcampus.ac.za>
2024-10-30 14:53:13 +0200

    first button added

--- PRG 272 Project/Form1.Designer.cs
+++ PRG 272 Project/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(74, 55);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(213, 94);
+            this.btnAddStudent.TabIndex = 0;
+            this.btnAddStudent.Text = "Add New Student";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1102, 587);
+            this.Controls.Add(this.btnAddStudent);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddStudent;
     }
 }
 
